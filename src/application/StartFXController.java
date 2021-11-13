3e90f5b51edfc379465574ebdb4b32e0ce622ec8
steffile <59478112+steffile@users.noreply.github.com>
2021-11-13 16:50:56 +0100

    Startfenster hinzugefuegt (#2)
    Co-authored-by: Stefanie S <s31315@bht-berlin.de>

---
+++ src/application/StartFXController.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.lang.ModuleLayer.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartFXController {
+
+	@FXML
+	private Button newGame, gamerules;
+	
+	//@FXML
+	/*public void openNewGame() {
+	 * 
+	 * }
+	 * 
+	 * public void howToPlay()
+	 * 
+	 * }
+	 */
+}
