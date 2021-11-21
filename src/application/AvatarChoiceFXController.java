20faa165bf5efaaf8e1d0735be804fe6dd9ef5e8
steffile <59478112+steffile@users.noreply.github.com>
2021-11-21 19:30:44 +0100

    Avatarauswahl hinzugefuegt (#4)
    Co-authored-by: steffile <s31315@bht-berlin.de>

---
+++ src/application/AvatarChoiceFXController.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.lang.ModuleLayer.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AvatarChoiceFXController {
+
+	@FXML
+	private Button btnCell, btnVirus;
+	
+	//@FXML
+	
+}
